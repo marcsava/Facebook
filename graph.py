4c6d8ca3c71744274ff85b564ef65707a72cf59c
marcsava <marcosavarese99@gmail.com>
2021-12-14 17:17:19 +0100

    COMMITTINO

--- graph.py
+++ graph.py
@@ -1,29 +1,14 @@
-"""Base class for undirected graphs.
-
-The Graph class allows any hashable object as a node
-and can associate key/value attribute pairs with each undirected edge.
-
-Self-loops are allowed but multiple edges are not (see MultiGraph).
-
-For directed graphs see DiGraph and MultiDiGraph.
-"""
-
 from reportviews import *
 from exception import *
 __all__ = ["Graph"]
 
-
 class Graph:
-
     node_dict_factory = dict
     node_attr_dict_factory = dict
     adjlist_outer_dict_factory = dict
     adjlist_inner_dict_factory = dict
     edge_attr_dict_factory = dict
     graph_attr_dict_factory = dict
-
-   # def to_directed_class(self):
-    #    return nx.DiGraph
 
     def to_undirected_class(self):
          return Graph
@@ -39,15 +24,8 @@ class Graph:
         self.graph = self.graph_attr_dict_factory()  # dictionary for graph attributes
         self._node = self.node_dict_factory()  # empty node attribute dict
         self._adj = self.adjlist_outer_dict_factory()  # empty adjacency dict
-        # attempt to load graph with data
-     #   if incoming_graph_data is not None:
-     #       convert.to_networkx_graph(incoming_graph_data, create_using=self)
 
         self.graph.update(attr)
-
-  #  @property
-  #  def adj(self):
-   #     return AdjacencyView(self._adj)
 
     @property
     def name(self):
@@ -91,45 +69,6 @@ class Graph:
         else:  # update attr even if node already exists
             self._node[node_for_adding].update(attr)
 
-    def add_nodes_from(self, nodes_for_adding, **attr):
-        for n in nodes_for_adding:
-            try:
-                newnode = n not in self._node
-                newdict = attr
-            except TypeError:
-                n, ndict = n
-                newnode = n not in self._node
-                newdict = attr.copy()
-                newdict.update(ndict)
-            if newnode:
-                if n is None:
-                    raise ValueError("None cannot be a node")
-                self._adj[n] = self.adjlist_inner_dict_factory()
-                self._node[n] = self.node_attr_dict_factory()
-            self._node[n].update(newdict)
-
-    def remove_node(self, n):
-        adj = self._adj
-        try:
-            nbrs = list(adj[n])  # list handles self-loops (allows mutation)
-            del self._node[n]
-        except KeyError as err:  # NetworkXError if n not in self
-            raise NetworkXError(f"The node {n} is not in the graph.") from err
-        for u in nbrs:
-            del adj[u][n]  # remove all edges n-u in graph
-        del adj[n]  # now remove node
-
-    def remove_nodes_from(self, nodes):
-        adj = self._adj
-        for n in nodes:
-            try:
-                del self._node[n]
-                for u in list(adj[n]):  # list handles self-loops
-                    del adj[u][n]  # (allows mutation of dict in loop)
-                del adj[n]
-            except KeyError:
-                pass
-
     @property
     def nodes(self):
         nodes = NodeView(self)
@@ -169,132 +108,9 @@ class Graph:
         self._adj[u][v] = datadict
         self._adj[v][u] = datadict
 
-    def add_edges_from(self, ebunch_to_add, **attr):
-        for e in ebunch_to_add:
-            ne = len(e)
-            if ne == 3:
-                u, v, dd = e
-            elif ne == 2:
-                u, v = e
-                dd = {}  # doesn't need edge_attr_dict_factory
-            else:
-                raise NetworkXError(f"Edge tuple {e} must be a 2-tuple or 3-tuple.")
-            if u not in self._node:
-                if u is None:
-                    raise ValueError("None cannot be a node")
-                self._adj[u] = self.adjlist_inner_dict_factory()
-                self._node[u] = self.node_attr_dict_factory()
-            if v not in self._node:
-                if v is None:
-                    raise ValueError("None cannot be a node")
-                self._adj[v] = self.adjlist_inner_dict_factory()
-                self._node[v] = self.node_attr_dict_factory()
-            datadict = self._adj[u].get(v, self.edge_attr_dict_factory())
-            datadict.update(attr)
-            datadict.update(dd)
-            self._adj[u][v] = datadict
-            self._adj[v][u] = datadict
-
-    def add_weighted_edges_from(self, ebunch_to_add, weight="weight", **attr):
-        self.add_edges_from(((u, v, {weight: d}) for u, v, d in ebunch_to_add), **attr)
-
-    def remove_edge(self, u, v):
-        try:
-            del self._adj[u][v]
-            if u != v:  # self-loop needs only one entry removed
-                del self._adj[v][u]
-        except KeyError as err:
-            raise NetworkXError(f"The edge {u}-{v} is not in the graph") from err
-
-    def remove_edges_from(self, ebunch):
-        adj = self._adj
-        for e in ebunch:
-            u, v = e[:2]  # ignore edge data if present
-            if u in adj and v in adj[u]:
-                del adj[u][v]
-                if u != v:  # self loop needs only one entry removed
-                    del adj[v][u]
-
-    def update(self, edges=None, nodes=None):
-        if edges is not None:
-            if nodes is not None:
-                self.add_nodes_from(nodes)
-                self.add_edges_from(edges)
-            else:
-                # check if edges is a Graph object
-                try:
-                    graph_nodes = edges.nodes
-                    graph_edges = edges.edges
-                except AttributeError:
-                    # edge not Graph-like
-                    self.add_edges_from(edges)
-                else:  # edges is Graph-like
-                    self.add_nodes_from(graph_nodes.data())
-                    self.add_edges_from(graph_edges.data())
-                    self.graph.update(edges.graph)
-        elif nodes is not None:
-            self.add_nodes_from(nodes)
-        else:
-            raise NetworkXError("update needs nodes or edges input")
-
-    def has_edge(self, u, v):
-        try:
-            return v in self._adj[u]
-        except KeyError:
-            return False
-
-    def neighbors(self, n):
-        try:
-            return iter(self._adj[n])
-        except KeyError as err:
-            raise NetworkXError(f"The node {n} is not in the graph.") from err
-
     @property
     def edges(self):
         return EdgeView(self)
-
-    def get_edge_data(self, u, v, default=None):
-        try:
-            return self._adj[u][v]
-        except KeyError:
-            return default
-
-    def adjacency(self):
-        return iter(self._adj.items())
-
-    @property
-    def degree(self):
-        return DegreeView(self)
-
-    def clear(self):
-        self._adj.clear()
-        self._node.clear()
-        self.graph.clear()
-
-    def clear_edges(self):
-        for neighbours_dict in self._adj.values():
-            neighbours_dict.clear()
-
-    def is_multigraph(self):
-        return False
-
-    def is_directed(self):
-        return False
-
-    def size(self, weight=None):
-        s = sum(d for v, d in self.degree(weight=weight))
-        # If `weight` is None, the sum of the degrees is guaranteed to be
-        # even, so we can perform integer division and hence return an
-        # integer. Otherwise, the sum of the weighted degrees is not
-        # guaranteed to be an integer, so we perform "real" division.
-        return s // 2 if weight is None else s / 2
-
-    def number_of_edges(self, u=None, v=None):
-        if u is None:
-            return int(self.size())
-        if v in self._adj[u]:
-            return 1
-        return 0
 
     def nbunch_iter(self, nbunch=None):
         if nbunch is None:  # include all nodes via iterator
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graph import *
 from maxcut import *
 
-
+# graf of marc
 G = Graph()
 G.add_edge('A', 'B', weight=20)
 G.add_edge('A', 'C', weight=4)
@@ -10,5 +10,14 @@ G.add_edge('C', 'D', weight=5)
 G.add_edge('D', 'E', weight=300)
 G.add_edge('A', 'D', weight=94)
 current, partition = one_exchange(G, weight='weight')
-
 print(current, partition)
+
+#graf of professores
+P = Graph()
+P.add_edge('A', 'B', weight=2)
+P.add_edge('A', 'C', weight=4)
+P.add_edge('B', 'D', weight=3)
+P.add_edge('C', 'D', weight=5)
+P.add_edge('D', 'E', weight=3)
+current2, partition2 = one_exchange(P, weight='weight')
+print(current2, partition2)
