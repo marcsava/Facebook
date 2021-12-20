89e6c479ada6511f5ee230e5fc160d71ab2e08c3
marcsava <marcosavarese99@gmail.com>
2021-12-20 11:22:55 +0100

    last3

--- delivered/graph.py
+++ delivered/graph.py
@@ -80,30 +80,30 @@ class Graph:
   # ------------------------- nested MyQueue class -------------------------
   class MyQUEUE:  # just an implementation of a queue
 
-      def __init__(self):
-        self.holder = []
+     def __init__(self):
+       self.holder = []
 
-      def enqueue(self, val):
-        self.holder.append(val)
+     def enqueue(self, val):
+       self.holder.append(val)
 
-      def dequeue(self):
-        val = None
-        try:
-          val = self.holder[0]
-          if len(self.holder) == 1:
-            self.holder = []
-          else:
-            self.holder = self.holder[1:]
-        except:
-          pass
+     def dequeue(self):
+       val = None
+       try:
+         val = self.holder[0]
+         if len(self.holder) == 1:
+           self.holder = []
+         else:
+           self.holder = self.holder[1:]
+       except:
+         pass
 
-        return val
+       return val
 
-      def IsEmpty(self):
-        result = False
-        if len(self.holder) == 0:
-          result = True
-        return result
+     def IsEmpty(self):
+       result = False
+       if len(self.holder) == 0:
+         result = True
+       return result
 
   #------------------------- Graph methods -------------------------
   def __init__(self, directed=False):
